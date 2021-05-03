850f226db252367da825bbd51fa45a5dac233db9
IvanGetman <i.getman90@gmail.com>
2021-05-03 17:50:47 +0300

    ДЗ N 2, добавлен bottom_navigation в основной макет

--- app/src/main/java/com/getman/myappfilmsearch/MainActivity.kt
+++ app/src/main/java/com/getman/myappfilmsearch/MainActivity.kt
@@ -10,9 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val helloText = findViewById<TextView>(R.id.hello_text)
-        val button = findViewById<Button>(R.id.button_text)
-        val helloWorld = ExampleDataClass( "Привет")
-        button.setOnClickListener { helloText.text = helloWorld.toString() }
     }
 }
